4aacc3a0a561ed36c2cb23be121e5d1909ed915f
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2020-12-26 21:18:10 +0300

    split code more

--- src/FourChanFull.js
+++ src/FourChanFull.js
@@ -4,14 +4,16 @@ const { defaultRequest } = require("./utils/defaultRequest.js");
 const { findBoard } = require("./utils/findBoard.js");
 
 const { parseStats } = require("./parsers/parseStats");
-const { parseSemiThread, parseThread } = require("./parsers/parseThread.js");
+const { parseThread } = require("./parsers/parseThread.js");
 const { parsePopularThread } = require("./parsers/parsePopularThread");
+const { parseBoard } = require("./parsers/parseBoard.js");
 
 const { SafetyType } = require("./other/SafetyType");
 const boards = require("./other/boards");
 
 const { Board } = require("./types/Board");
 const { PopularThread } = require("./types/PopularThread");
+
 
 class FourChanFull {
   #request = defaultRequest;
@@ -30,28 +32,10 @@ class FourChanFull {
   async board(boardCode, pageNumber = 1) {
     const board = findBoard(boardCode);
     if (!board) throw "Invalid board.";
-    const href = `https://boards.4chan.org/${boardCode}/${pageNumber == 1 ? "" : pageNumber
-      }`;
-    const document = new JSDOM(await this.#request("GET", href), { url: href })
-      .window.document;
+    const href = `https://boards.4chan.org/${boardCode}/${pageNumber == 1 ? "" : pageNumber}`;
+    const document = new JSDOM(await this.#request("GET", href), { url: href }).window.document;
 
-    const _board = new Board();
-
-    _board.banner = `https://s.4cdn.org/image/title/${document
-      .querySelector("#bannerCnt")
-      .getAttribute("data-src")}`;
-
-    _board.name = board.name;
-    _board.code = board.code;
-    _board.worksafe = board.worksafe;
-    _board.page = pageNumber;
-    _board.url = href;
-
-    _board.threads = Array.from(document.querySelectorAll(".thread")).map((threadElm) => {
-      return parseSemiThread(threadElm, board);
-    });
-
-    return _board;
+    return parseBoard(document, board);
   }
 
   async archive(boardCode) {
---
+++ src/parsers/parseBoard.js
@@ -0,0 +1,29 @@
+const { Board } = require("../types/Board");
+const { parseSemiThread } = require("./parseThread.js");
+
+/**
+ * @param {HTMLElement} e
+ * @param {{worksafe: Boolean, name: String, code: String}} board
+ * 
+ * @returns {Board}
+ */
+function parseBoard(e, board) {
+  const _board = new Board();
+
+  _board.banner = `https://s.4cdn.org/image/title/${e.querySelector("#bannerCnt").getAttribute("data-src")}`;
+
+  _board.name = board.name;
+  _board.code = board.code;
+  _board.worksafe = board.worksafe;
+  _board.page = pageNumber;
+  _board.url = href;
+
+  _board.threads = Array.from(e.querySelectorAll(".thread")).map((threadElm) => {
+    return parseSemiThread(threadElm, board);
+  });
+
+  return _board;
+
+}
+
+module.exports = { parseBoard }
